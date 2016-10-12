63de280448ee38ca2b32894b8d1a2b41571e599f
Melvin Canela <m.canela92@outlook.com>
2016-10-12 16:58:55 -0400

    added database settings

--- cultureiq/settings.py
+++ cultureiq/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from . import dbsettings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djng',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'cultureiq.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': dbsettings.NAME,
+        'USER': dbsettings.USER,
+        'PASSWORD': dbsettings.PASSWORD,
+        'HOST': dbsettings.HOST,
+        'PORT': dbsettings.PORT
     }
 }
 
